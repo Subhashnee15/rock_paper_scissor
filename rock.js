9a672c2190ed35bf57cf7b969a20fb9251d9eabd
SUBHASHNEE THAKUR <126400709+Subhashnee15@users.noreply.github.com>
2025-01-31 23:33:31 +0530

    Add files via upload

--- rock.js
+++ rock.js
@@ -15,7 +15,7 @@ const game = () => {
 
         // Function to start playing game 
         playerOPtion.forEach(option => {
-            option.addEventListener('click',funtion () {
+            option.addEventListener('click',function () {
                 const movesLeft = document.querySelector('.movesleft');
                 moves++;
                 movesLeft.innerText = `Moves Left: ${10- moves}`;
@@ -66,7 +66,56 @@ const game = () => {
                 playerScoreBoard.textContent = playerScore;
             }
         }
-        
-    }
+        else if (player == 'paper') {
+            if (computer == 'scissors') {
+                result.textContent = 'Computer Won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
 
-}
+            } else {
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }
+        }
+    }
+    // Function to run when game is over 
+    const gameOver = (playerOptions, movesLeft) => {
+        const chooseMove = document.querySelector('.move');
+        const result = document.querySelector('.result');
+        const reloadBtn = document.querySelector('.reload');
+        playerOptions.forEach(option => {
+            option.style.display = 'none';
+        })
+        chooseMove.innerText = 'Game Over!!'
+        movesLeft.style.display = 'none';
+
+        if (playerScore > computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = ' You Won The Game'
+            result.style.color = '#308D46';
+        }
+        else if (playerSCore < computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Lost the Game';
+            result.style.color = 'red';
+        }
+        else {
+            result.style.fontSize = '2rem';
+            result.innerText = 'Tie';
+            result.style.color = 'grey'
+        }
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display = 'flex'
+        reloadBtn.addEventListener('click' , () => {
+            window.location.reload();
+        })
+
+
+    }
+    //calling playgame function inside game 
+    playGame();
+} 
+// calling the game function
+game();
+
